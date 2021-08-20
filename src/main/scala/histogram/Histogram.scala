5e32ef197d1731fa43547b41d1e0ffb5c5cb2a53
mdrozdov <nik.thrush@gmail.com>
2021-08-20 20:38:48 +0300

    Code has been refactored after review

--- src/main/scala/histogram/Histogram.scala
+++ src/main/scala/histogram/Histogram.scala
@@ -10,27 +10,19 @@ object Histogram {
 
   def loadData(fileName: String): Try[ParSeq[String]] = {
     Using(Source.fromFile(fileName)) {
-      bufferedSource => {
-        bufferedSource.getLines().toSeq.par.flatMap(_.split("[^\\d\\w]+"))
-      }
+      _.getLines().toSeq.par.flatMap(_.split("[^\\d\\w]+"))
     }
   }
 
-  def saveData(fileName: String, counted: Seq[(String, Int)]): Unit = {
-    Using(new PrintWriter(fileName)) {
+  def saveData(fileName: String, counted: Seq[(String, Int)]): Unit = Using(new PrintWriter(fileName)) {
       writer => {
-        counted.foreach(word => writer.write(s"${word._1} : ${word._2}\n"))
+        counted.foreach{case (word, qty) => writer.write(s"$word : $qty\n")}
       }
     }
-  }
 
-  def countWords(words: ParSeq[String]): Seq[(String, Int)] = {
-    words.groupBy(_.toLowerCase).map(x => (x._1, x._2.length)).toSeq.seq.sortWith(_._2 > _._2)
-  }
+  def countWords(words: ParSeq[String]): Seq[(String, Int)] = words.groupBy(_.toLowerCase).map{case (word, qty) => (word, qty.length)}.toSeq.seq.sortWith(_._2 > _._2)
 
-  def printResult(counted: Seq[(String, Int)]): Unit = {
-    counted.foreach(word => println(s"${word._1} : ${word._2}"))
-  }
+  def printResult(counted: Seq[(String, Int)]): Unit = counted.foreach{case (word, qty) => println(s"$word : $qty")}
 
 }
 

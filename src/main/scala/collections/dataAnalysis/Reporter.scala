c886b0517431fbd6822d71173c3d430d7b280a8c
mdrozdov <nik.thrush@gmail.com>
2021-08-25 11:42:47 +0300

    Code has been reworked to use Monoid of Person. Added using of parallel collections

--- src/main/scala/collections/dataAnalysis/Reporter.scala
+++ src/main/scala/collections/dataAnalysis/Reporter.scala
@@ -1,19 +1,42 @@
 package collections.dataAnalysis
 
+import scala.collection.parallel.CollectionConverters._
+
 object Reporter {
 
+  sealed trait Monoid[T] {
+    def empty: T
+    def add(x: T, y: T): T
+  }
+
+  implicit object PersonMonoid extends Monoid[Person] {
+    override def empty: Person = Person("monoidPerson")
+    override def add(x: Person, y: Person): Person = Person("monoidPerson",
+      timePrimary = x.timePrimary + y.timePrimary,
+      timeWorking = x.timeWorking + y.timeWorking,
+      timeOther = x.timeOther + y.timeOther)
+  }
+
+  private def avgTimeWorking(st: Stream[Person]): Double = {
+    if (st.isEmpty) 0
+    else st.par.reduce((x, y) => PersonMonoid.add(x, y)).timeWorking.toDouble / st.length
+  }
+
+  private def avgTimeOther(st: Stream[Person]): Double = {
+    if (st.isEmpty) 0
+    else st.par.reduce((x, y) => PersonMonoid.add(x, y)).timeOther.toDouble / st.length
+  }
+
   def primaryToOtherTimeCorrelation(people: Stream[Person]): String = {
-    val (totalPrimary, totalOther) = people.map(p => (p.timePrimary, p.timeWorking + p.timeOther)).reduce((x, y) => (x._1 + y._1, x._2 + y._2))
-    val primPercent = BigDecimal(100.0 * totalPrimary.toDouble/ (totalPrimary + totalOther)).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+    val totalPerson = (PersonMonoid.empty #:: people).par.reduce((x, y) => PersonMonoid.add(x, y))
+    val primPercent = BigDecimal(100.0 * totalPerson.timePrimary.toDouble/ (totalPerson.timePrimary + totalPerson.timeWorking + totalPerson.timeOther)).setScale(2, BigDecimal.RoundingMode.HALF_UP)
     s"$primPercent% : ${100 - primPercent}%"
   }
 
   def womenAndMenWorkingTimeComparison(people: Stream[Person]): String = {
     val (female, male) = people.partition(_.gender == Gender.Female)
-    val avgFemaleWT = if (female.nonEmpty) female.map(_.timeWorking).sum.toDouble / female.length else 0.0
-    val avgMaleWT = if (male.nonEmpty) male.map(_.timeWorking).sum.toDouble / male.length else 0.0
-    val avgFemale = BigDecimal(avgFemaleWT).setScale(2, BigDecimal.RoundingMode.HALF_UP)
-    val avgMale = BigDecimal(avgMaleWT).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+    val avgFemale = BigDecimal(avgTimeWorking(female)).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+    val avgMale = BigDecimal(avgTimeWorking(male)).setScale(2, BigDecimal.RoundingMode.HALF_UP)
     (avgFemale, avgMale) match {
       case (w, m) if w > m => s"No, in average women spend more time to work ($w) then men ($m)"
       case (w, m) if w < m => s"No, in average men spend more time to work ($m) then women ($w)"
@@ -22,16 +45,10 @@ object Reporter {
   }
 
   def diffAgePeriodPrimaryTimeComparison(people: Stream[Person]): String = {
-    val (avgYoungPT, _, avgActivePT, _, avgElderPT, _) = people.foldLeft((0.0, 1.0, 0.0, 1.0, 0.0, 1.0))((acc, person) =>
-      person.agePeriod match {
-        case AgePeriod.Young => (acc._1 + (person.timePrimary - acc._1) / acc._2, acc._2 + 1, acc._3, acc._4, acc._5, acc._6)
-        case AgePeriod.Active => (acc._1, acc._2, acc._3 + (person.timeWorking - acc._3) / acc._4, acc._4 + 1, acc._5, acc._6)
-        case AgePeriod.Elder => (acc._1, acc._2, acc._3, acc._4, acc._5 + (person.timeWorking - acc._5) / acc._6, acc._6 + 1)
-      }
-    )
-    val avgYoung = BigDecimal(avgYoungPT).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
-    val avgActive = BigDecimal(avgActivePT).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
-    val avgElder = BigDecimal(avgElderPT).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+    val peopleByAge = people.groupBy(_.agePeriod)
+    val avgYoung = BigDecimal(avgTimeWorking(peopleByAge(AgePeriod.Young))).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+    val avgActive = BigDecimal(avgTimeWorking(peopleByAge(AgePeriod.Active))).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+    val avgElder = BigDecimal(avgTimeWorking(peopleByAge(AgePeriod.Elder))).setScale(2, BigDecimal.RoundingMode.HALF_UP)
     (avgYoung, avgActive, avgElder) match {
       case (y, a, e) if y == a & a == e => s"No, average time spent on primary needs are same for all people: $y"
       case (y, a, e) => s"Yes, there is different average time spent on primary needs for diff age periods: Young = $y, Active = $a, Elder = $e"
@@ -39,13 +56,10 @@ object Reporter {
   }
 
   def employedToUnemployedOtherTimeCorrelation(people: Stream[Person]): String = {
-    val (totalEmployed, _, totalUnemployed, _) = people.foldLeft((0.0, 1.0, 0.0, 1.0))((acc, person) =>
-      person.workStatus match {
-        case WorkStatus.Working => (acc._1 + (person.timeOther - acc._1) / acc._2, acc._2 + 1, acc._3, acc._4)
-        case _ => (acc._1, acc._2, acc._3 + (person.timeOther - acc._3) / acc._4, acc._4 + 1)
-      }
-    )
-    val emplPercent = BigDecimal(100 * totalEmployed/ (totalEmployed + totalUnemployed)).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+    val (employed, unemployed) = people.partition(_.workStatus == WorkStatus.Working)
+    val avgEmployedTO = avgTimeOther(employed)
+    val avgUnemployedTO = avgTimeOther(unemployed)
+    val emplPercent = BigDecimal(100 * avgEmployedTO/ (avgEmployedTO + avgUnemployedTO)).setScale(2, BigDecimal.RoundingMode.HALF_UP)
     s"$emplPercent% : ${100 - emplPercent}%"
   }
 
